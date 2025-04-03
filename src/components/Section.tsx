6585f16e7e5363678473dad692cf6afe27d66eb0
Michael Vogtherr <vogtherr.michael@gmail.com>
2025-04-03 22:17:33 +0200

    Fix Section props type

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,4 +1,14 @@
-export default function Section({title, children, ...props}: {title: string, children: React.ReactNode, [key: string]: React.HTMLAttributes<HTMLElement>}): JSX.Element {
+export default function Section(
+    {
+        title,
+        children,
+        ...props
+    }: {
+        title: string,
+        children: React.ReactNode,
+        [key: string]: any
+    }
+): JSX.Element {
     return (
         <section {...props}>
             <h2>{title}</h2>
