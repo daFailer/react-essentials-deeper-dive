5054a693960661280ba7c07edc4348461f7a5c10
Michael Vogtherr <vogtherr.michael@gmail.com>
2025-04-03 21:44:15 +0200

    Replace unnecessary element by using Fragment

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
+import { Fragment } from 'react';
 import Header from './components/Header/Header';
 import CoreConcept from './components/CoreConcept/CoreConcept';
 import Examples from './components/Examples/Examples';
 
 function App(): JSX.Element {
   return (
-    <div>
+    <Fragment>
       <Header />
       <main>
         <CoreConcept />
         <Examples />
       </main>
-    </div>
+    </Fragment>
   );
 }
 
