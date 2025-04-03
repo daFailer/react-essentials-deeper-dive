c8732bc1a67fee398605e4e806259c6a497970a7
Michael Vogtherr <vogtherr.michael@gmail.com>
2025-04-03 21:45:10 +0200

    Replace Fragment by more mordern alternative

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import { Fragment } from 'react';
 import Header from './components/Header/Header';
 import CoreConcept from './components/CoreConcept/CoreConcept';
 import Examples from './components/Examples/Examples';
 
 function App(): JSX.Element {
   return (
-    <Fragment>
+    <>
       <Header />
       <main>
         <CoreConcept />
         <Examples />
       </main>
-    </Fragment>
+    </>
   );
 }
 
